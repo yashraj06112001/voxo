dcb73036d9905956f7fd25bcbd1a7b3bfe4cf7b3
yash raj mishra <yashraj06112001@gmail.com>
2026-03-20 03:29:25 +0530

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI!),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
