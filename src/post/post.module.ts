1d3aacea1d2894bb5876a962735c9fc4e1333bfc
yash raj mishra <109572402+yashraj06112001@users.noreply.github.com>
2026-04-14 23:28:23 +0530

    Add post module: src/post/post.module.ts

---
+++ src/post/post.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostService } from './post.service';
+import { PostController } from './post.controller';
+import { PostSchema } from './entities/post.entity';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Post', schema: PostSchema }])],
+  controllers: [PostController],
+  providers: [PostService],
+})
+export class PostModule {}
